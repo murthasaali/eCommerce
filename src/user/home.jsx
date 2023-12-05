cd92ccc3c8070dce957412e01e4695a289937e8f
murtasaali <murthasaalick123@gmail.com>
2023-12-05 12:02:57 +0530

    push60

--- src/user/home.jsx
+++ src/user/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import Navber from './navbar'
 import Mainbar from './mainbar'
 import { motion } from 'framer-motion';
@@ -15,11 +15,29 @@ import broast from '../arrangement-with-chicken-food-white-background_807701-473
 import pizza from '../italian-pizza-fast-food-icon-isolated-3d-render-illustration_439185-12977-removebg-preview.png'
 import shavaya from '../realistic-illustration-roasted-turkey-grilled-chicken-with-slices-spices-vegetable_1441-1865-removebg-preview.png'
 import BasicCard from '../components/card';
-import { FaFacebook, FaInstagram, FaMailBulk, FaPhone, FaWhatsapp } from 'react-icons/fa';
+import { FaFacebook, FaInstagram, FaMailBulk, FaPhone, FaSearch, FaWhatsapp } from 'react-icons/fa';
 import { selectIsLoading, selectIslogin, setIsLoading } from '../redux/authSlice';
 import Loading from '../components/loading';
 import { Modal } from '@mui/material';
 import Location from '../components/location';
+
+import {  setProducts } from '../redux/authSlice';
+import axiosInsatnce from '../axiosInstance/instance';
+import { selectToken, selectProducts } from '../redux/authSlice';
+import VoiceSearchExample from '../components/voiceSearch';
+
+
+const style = {
+  position: 'absolute' ,
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'black',
+  boxShadow: '2px 5px 10px 5px white',
+  boxShadow: 24,
+  p: 4,
+};
 const container = {
   hidden: { opacity: 1, scale: 0 },
   visible: {
@@ -40,14 +58,12 @@ const items = {
   }
 };
 function Home() {
-  const dispatch=useDispatch()
   console.log("loadingggg")
   const [offerModal,setOfferModal]=useState(false)
   const isLoading= useSelector(selectIsLoading)
   setTimeout(() => {
     dispatch(setIsLoading(false))
   }, 3000);
-  const nav = useNavigate()
   const menucard=[
     {title:"pizza",src:`${pizza}`,nav:'/kitchen/pizza'},
     {title:"burger",src:`${burger}`,nav:'/kitchen/burger'},
@@ -59,12 +75,100 @@ function Home() {
     {title:"shrimp",src:`${shrimp}`,nav:'/kitchen/shrimp'},
    
 ]
+const isLogin=useSelector(selectIslogin)
+const [loginModal, setloginModal] = useState(false);
+
+const [modal, setModal] = useState(false);
+const [searchQuery, setSearchQuery] = useState([]);
+const token = useSelector(selectToken);
+const products = useSelector(selectProducts);
+const dispatch = useDispatch();
+const nav=useNavigate()
+// Function to filter products based on search query
+const apiKey=process.env.REACT_APP_API_KEY
+const getAllProducts = async (token,apiKey) => {
+  try {
+    const response = await axiosInsatnce.get(`/products?accessKey=${apiKey}`, {
+     
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const { status, message, data } = response.data;
+    if (status === 'success') {
+      // Successfully fetched products.
+      dispatch(setProducts(data)); // Use setProductsAction instead of setProducts
+      console.log('navbar:', data);
+    } else {
+      console.error('Product retrieval failed. Message:', message);
+    }
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+};
+useEffect(() => {
+  getAllProducts(token,apiKey);
+}, [token]);
+
+const handleInputChange = (e) => {
+  const text=e.target.value;
+  const text1=text.trim().toLocaleLowerCase()
+  const results = products.filter((product) => product.title.toLowerCase().includes(text1));
+  console.log(results);
+  setSearchQuery(results);
+};
+const toLogin=()=>{
+  setloginModal(false)
+  nav('/login')
+}
+
 
 
   return (
     <div className='home_main'
  
     >
+            {modal && (
+        <Modal
+          open={modal}
+          onClose={() => setModal(false)}
+          style={{
+            display: 'flex',
+            alignItems: 'start',
+            justifyContent: 'center',
+            marginTop: '10px',
+          }}
+        >
+          <div className="p-8 flex flex-col justify-center items-center gap-2 w-full rounded-xl">
+            <input
+              type="text"
+              className="bg-stone-300 text-blue-950 w-3/4 h-8 rounded-3xl pl-4 overflow-hidden shadow-md"
+              style={{ opacity: 1, y: 0 }}
+              onChange={handleInputChange}
+             
+              placeholder="Search products..."
+            />
+             <div className="bg-orange-400 opacity-75 h-auto w-3/4 rounded-lg">
+              
+              <ul >
+             
+  {searchQuery.length>0?
+ (   searchQuery.map((value,index)=>{
+      return(
+
+      <p key={index} className=' font-mono rounded-lg ' onClick={()=>nav(`/viewproduct/${value._id}`)} >
+        {index+1}:
+{value.title}
+      </p>
+    )})):<div>plsss enter something</div>
+  }
+              </ul>
+            </div>
+              <div>
+              </div>
+          </div>
+        </Modal>
+      )}
       {
         isLoading&&(<Loading/>)
       }
@@ -116,7 +220,7 @@ function Home() {
   </p>
 <Carousal />
   </div>
-      <div className=' p-4 font-thin h-1/2 lg:h-full'> <p>choose by menu</p><motion.ul
+      <div className=' p-4 font-thin h-1/2 lg:h-full flex flex-col gap-4'> <div className='flex justify-center items-center gap-8'>choose by menu <FaSearch className='text-xl  flex md:hidden' onClick={()=>setModal(true)}/> </div > <motion.ul
     className="w-full h-full sm:h-1/2 lg:h-full p-2 grid grid-cols-4 gap-4 grid-rows-2 gap-15 p-15 overflow-hidden bg-opacity-20 rounded-lg"
     variants={container}
     initial="hidden"
