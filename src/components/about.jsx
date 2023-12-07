624e48b85f6288e1c442ce83ad93d6206c53a20f
murtasaali <murthasaalick123@gmail.com>
2023-12-07 21:43:05 +0530

    push70

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const About = () => {
   return (
       <section className="w-full h-56 bg-white">
-        <div className='bg-black h-20 w-20'>
+      about<div className='bg-black h-20 w-20'>
 
         </div>
 
